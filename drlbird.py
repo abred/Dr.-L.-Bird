29cb98412c36efd7eb62e49cb3f75413c70cbd4d
Peter Hirsch <phirsch@CLTest.zih.tu-dresden.de>
2016-09-21 15:39:05 +0200

    dspg

--- drlbird.py
+++ drlbird.py
@@ -8,6 +8,7 @@ from driver import Driver
 
 from gaussianPolicy import GaussianPolicy
 from ddpgPolicy import DDPGPolicy
+from dspgPolicy import DSPGPolicy
 
 from ouNoise import OUNoise
 from replay_buffer import ReplayBuffer
@@ -39,6 +40,85 @@ class DrLBird(Driver):
             replayBufferSize = 1000
             miniBatchSize = 8
             gamma = 0.99
+            replay = ReplayBuffer(replayBufferSize)
+
+            for e in range(maxEpisodes):
+                # noise = OUNoise(3)
+                noise = OUNoise(3, sigma=[1.5, 180.0, 90.0])
+                oldScore = 0
+                terminal = False
+                ep_reward = 0
+                ep_ave_max_q = 0
+                self.loadRandLevel()
+
+                step = 0
+                while not terminal:
+                    step += 1
+                    self.fillObs()
+                    state = self.preprocessDataForNN()
+                    action, a_scaled = self.policy.getAction(state,
+                                                             noise.noise())
+                    score, terminal, newState = self.actionResponse(a_scaled)
+                    reward = score - oldScore
+                    reward *= 0.0001
+                    oldScore = score
+                    replay.add(state, action, reward, terminal, newState)
+
+                    if replay.size() > miniBatchSize:
+                        s_batch, a_batch, r_batch, t_batch, ns_batch =\
+                            replay.sample_batch(miniBatchSize)
+
+                        qValsNewState = self.policy.predict_target_nn(ns_batch)
+                        y_batch = np.zeros((miniBatchSize, 1))
+                        for i in range(miniBatchSize):
+                            if t_batch[i] == terminal:
+                                y_batch[i] = r_batch[i]
+                            else:
+                                y_batch[i] = r_batch[i] + \
+                                    gamma * qValsNewState[i]
+
+                        ep_ave_max_q += np.amax(qValsNewState)
+
+                        self.policy.update(s_batch, a_batch, y_batch)
+                        self.policy.update_targets()
+
+                    ep_reward += reward
+
+                summary_str = sess.run(summary_ops, feed_dict={
+                    summary_vars[0]: ep_reward,
+                    summary_vars[1]: ep_ave_max_q / float(step)
+                })
+
+                writer.add_summary(summary_str, e)
+                writer.flush()
+
+                print('| Reward: {}, | Episode {}, | Qmax: {}'.
+                      format(ep_reward, e,
+                             ep_ave_max_q / float(step)))
+
+    def DSPG(self):
+        with tf.Session() as sess:
+            episode_reward = tf.Variable(0., name="episodeReward")
+            tf.scalar_summary("Reward", episode_reward)
+            episode_ave_max_q = tf.Variable(0., name='epsideAvgMaxQ')
+            tf.scalar_summary("Qmax Value", episode_ave_max_q)
+            summary_vars = [episode_reward, episode_ave_max_q]
+            summary_ops = tf.merge_all_summaries()
+
+            timestamp = str(int(time.time()))
+            out_dir = os.path.abspath(os.path.join(os.path.curdir,
+                                                   "runs", timestamp))
+            print("Summaries will be written to: {}\n".format(out_dir))
+
+            self.policy = DSPGPolicy(sess, out_dir)
+            writer = tf.train.SummaryWriter(out_dir, sess.graph)
+
+            sess.run(tf.initialize_all_variables())
+
+            maxEpisodes = 1000
+            replayBufferSize = 1000
+            miniBatchSize = 18
+            gamma = 0.999
             replay = ReplayBuffer(replayBufferSize)
 
             for e in range(maxEpisodes):
---
+++ dspgPolicy.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+from dspgActor import Actor
+
+from dspgCritic import Critic
+
+
+class DSPGPolicy:
+    def __init__(self, sess, out_dir, resume=False):
+        self.actor = Actor(sess, out_dir)
+        self.critic = Critic(sess, out_dir)
+
+    def getAction(self, state, noise):
+        a = self.actor.run_predict(state)
+        a_scaled = np.copy(a)
+        a_scaled[0][0] *= 50.0
+        a_scaled[0][1] *= 9000.0
+        a_scaled[0][2] *= 4000.0
+        print("Next action: {}\n".format(a))
+        print("Next action(scaled): {}\n".format(a_scaled))
+        a_scaled +=  noise
+        print("Next action(noised): {}\n".format(a_scaled))
+
+        return a, a_scaled
+
+    def update(self, states, actions, targets):
+        step = self.critic.run_train(states, actions, targets)
+        ac = self.actor.run_predict(states)
+        a_grad = self.critic.run_get_action_gradients(states, ac)
+        print("action gradients: {}".format(a_grad[0]))
+        self.actor.run_train(states, a_grad[0], step)
+
+    def update_targets(self):
+        self.critic.run_update_target_nn()
+        self.actor.run_update_target_nn()
+
+    def predict_target_nn(self, state):
+        a = self.actor.run_predict_target(state)
+        return self.critic.run_predict_target(state, a)
