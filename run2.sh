f84843e9fb720289f5cbced118e0a0e51e106461
Peter Hirsch <peter.hirsch@tu-dresden.de>
2018-04-24 18:12:14 +0200

    path fixes

--- run2.sh
+++ run2.sh
@@ -6,7 +6,7 @@
 #SBATCH -n 2
 #SBATCH -N 1
 #SBATCH -c 1
-#SBATCH --time 0-12:00:00
+#SBATCH --time 0-01:00:00
 ###SBATCH --time 0-01:00:00
 #SBATCH --mem 8G
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT_90
@@ -16,11 +16,11 @@
 
 echo $SLURMD_NODENAME > /scratch/s7550245/Dr.-L.-Bird/host-$SLURM_JOB_ID
 rm /scratch/s7550245/chromiums/chromium$3/SingletonLock
-cp -r  ~/Application\ Cache /scratch/s7550245/chromiums/chromium$3/Default
+cp -r  ~/aibird_stuff/Application\ Cache /scratch/s7550245/chromiums/chromium$3/Default
 # cp -r  ~/Application\ CacheBackup/* /scratch/s7550245/chromium/Default/Application\ Cache
 
 
-cd ~/abV1.32/plugin
+cd ~/aibird_stuff/abV1.32/plugin
 
 export DISPLAY=:1
 Xvfb :1 -screen 0 1024x768x16 &
@@ -29,9 +29,9 @@ Xvfb :1 -screen 0 1024x768x16 &
 portS=$1
 portC=$2
 
-cp -r /home/s7550245/abV1.32/plugin /scratch/s7550245/chromiums/chromium$3
+cp -r /home/s7550245/aibird_stuff/abV1.32/plugin /scratch/s7550245/chromiums/chromium$3
 # cp /home/s7550245/abV1.32/plugin/script.jsBackup /scratch/s7550245/chromiums/chromium$3/plugin/script.js
 sed -i "s/localhost:9000/localhost:$portS/g" /scratch/s7550245/chromiums/chromium$3/plugin/script.js
 ../../AppDir51/bin/ld-linux-x86-64.so.2 --library-path ../../AppDir51/bin ../../AppDir51/bin/chrome --disable-setuid-sandbox --single-process --disable-gpu --load-extension=/scratch/s7550245/chromiums/chromium$3/plugin --always-authorize-plugins --disable-infobars --disable-session-crashed-bubble  --user-data-dir=/scratch/s7550245/chromiums/chromium$3  http://chrome.angrybirds.com  &
 
-/sw/global/tools/java/jdk1.8.0_66/bin/java -jar ~/Dr.-L.-Bird2/ABServer.jar -s $portS -c $portC
+/sw/global/tools/java/jdk1.8.0_66/bin/java -jar ~/aibird_stuff/Dr.-L.-Bird/ABServer.jar -s $portS -c $portC
--- run3.sh
+++ run3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-srun -A p_argumentation -N 1 --ntasks=1 -c 1 --mem=4G --time=1:00:00 -w taurusi1029 --pty --x11=first zsh
-cd ~/AppDirx11vnc
+#srun -A p_cvldpose -N 1 --ntasks=1 -c 1 --mem=4G --time=1:00:00 -w taurusi1029 --pty --x11=first zsh
+cd ~/aibird_stuff/AppDirx11vnc
 bin/ld-linux-x86-64.so.2 --library-path bin bin/x11vnc -display :1 -localhost &
 vncviewer :0
